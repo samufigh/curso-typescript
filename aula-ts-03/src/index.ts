e91399ef953b7273a57e8688320eaeebeb9c66f2
samufigh <samufigh21@gmail.com>
2023-09-14 14:00:45 -0300

    exercícios 04 - 08

--- aula-ts-03/src/index.ts
+++ aula-ts-03/src/index.ts
@@ -1,16 +1,16 @@
-function multiply(num1: number, num2: number) {
+function multiply(num1: number, num2: number) :number {
     return num1 * num2;
 }
 
-function sum(num1:number, num2:number) {
+function sum(num1:number, num2:number) :number {
     return num1 + num2;
 }
 
-function isEven(num:number) {
+function isEven(num:number) : boolean {
     return num % 2 === 0;
 }
 
-function showResult(result:number) {
+function showResult(result:number): void {
     if(isEven(result)) {
         console.log(`The result is ${result} and it's even!`);
     } else {
---
+++ aula-ts-04/src/index.ts
@@ -0,0 +1,32 @@
+function multiply(num1: number, num2: number) {
+    return num1 * num2;
+}
+
+function sum(num1: number, num2: number) {
+    return num1 + num2;
+}
+
+function isEven(num :number) {
+    return num % 2 === 0;
+}
+
+function showResult(result: number) {
+    if(isEven(result)) {
+        console.log(`The result is ${result} and it's even!`);
+    } else {
+        console.log(`The result is ${result} and it's odd!`);
+    }
+}
+
+(() => {
+    const num1 = prompt("First Number");
+    const num2 = prompt("Second Number");
+    
+    if (typeof num1 === 'number' && typeof num2 === 'number'){
+        let result = sum(num1,num2);
+        result += multiply(1,2);
+        showResult(result);
+    } else {
+        console.log("impossible to execute the function with these inputs")
+    }
+})();
