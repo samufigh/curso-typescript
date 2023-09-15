03048f43f3b9a51bea640211eb920c6c31376814
samufigh <samufigh21@gmail.com>
2023-09-15 20:01:53 -0300

    Revert "exercício 12"

--- aula-ts-10/index.ts
+++
@@ -1,7 +0,0 @@
-function identity<T>(value: T): T {
-  return value;
-}
-
-console.log(identity<number>(42)); // 42
-console.log(identity<string>("Hello")); // Hello
-console.log(identity<number[]>([1, 2, 3])); // [1, 2, 3]
